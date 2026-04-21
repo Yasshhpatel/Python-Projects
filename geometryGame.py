95c227b79e20139e951676c86c92ea9114984b94
Yash Patel <yashakshar31@gmail.com>
2026-04-21 23:17:39 +0530

    Sample Code

--- geometryGame.py
+++ geometryGame.py
@@ -1,2 +1,16 @@
 # Swami Shriji
-print(" Geometry Game ")
+print("Geometry Game")
+
+class point:
+
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+
+p1 = point(10,20)
+print(type(p1))
+
+
+num1 = int("2")
+print(type(num1))
